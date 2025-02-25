c514a17948baf2ee59e248a58c4108765305ba0b
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-02-24 22:15:45 -0300

    feature - created endpoint to extract excel table by csv

--- src/main/java/com/guilhermezuriel/exceltablemaker/controller/ExcelMakerController.java
+++ src/main/java/com/guilhermezuriel/exceltablemaker/controller/ExcelMakerController.java
@@ -5,12 +5,10 @@ import com.guilhermezuriel.exceltablemaker.service.excel.dtos.RequestExcelTable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("v1")
@@ -28,6 +26,15 @@ public class ExcelMakerController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
         headers.setContentDispositionFormData("attachment", requestExcelTable.name()+".xlsx");
+        return ResponseEntity.ok().headers(headers).body(inputStream);
+    }
+
+    @PostMapping("excel")
+    public ResponseEntity<byte[]> createExcelByCsvFile(@RequestBody InputStream inputStream, @RequestParam String tableName) throws IOException {
+        var inputStream = this.excelService.createExcelByRequest();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", tableName +".xlsx");
         return ResponseEntity.ok().headers(headers).body(inputStream);
     }
 }
